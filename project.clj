1f1293b8e1c8680a8497419b93871c688625a197
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-01-04 17:43:09 +0100

    import

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject spootnik/kinsky "0.1.1"
+  :description "Kafka processing library"
+  :dependencies [[org.clojure/clojure            "1.7.0"]
+                 [org.clojure/tools.logging      "0.3.1"]
+                 [org.clojure/core.async         "0.2.374"]
+                 [org.apache.kafka/kafka-clients "0.9.0.0"]
+                 [cheshire                       "5.5.0"]])
