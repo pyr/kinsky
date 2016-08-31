65ade01a9ff657845ef712814470ded3cb6bef24
Pierre-Yves Ritschard <pyr@spootnik.org>
2016-08-31 11:17:11 +0200

    project: now target 0.10.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/kinsky "0.1.15-SNAPSHOT"
+(defproject spootnik/kinsky "0.1.15"
   :description "Kafka clojure client library"
   :plugins [[lein-codox "0.9.1"]]
   :url "https://github.com/pyr/kinsky"
@@ -8,5 +8,5 @@
           :metadata   {:doc/format :markdown}}
   :dependencies [[org.clojure/clojure            "1.8.0"]
                  [org.clojure/core.async         "0.2.385"]
-                 [org.apache.kafka/kafka-clients "0.9.0.1"]
+                 [org.apache.kafka/kafka-clients "0.10.0.1"]
                  [cheshire                       "5.6.3"]])
