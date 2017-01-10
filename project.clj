26e820272b92bafa6c43962677646ea429cff394
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-01-10 09:54:18 +0100

    project: better late than never

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/kinsky "0.1.15"
+(defproject spootnik/kinsky "0.1.16-SNAPSHOT"
   :description "Kafka clojure client library"
   :plugins [[lein-codox "0.9.1"]]
   :url "https://github.com/pyr/kinsky"
