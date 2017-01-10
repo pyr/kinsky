6150db004524d0af9327e360e0a23cd63cb3a47a
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-01-10 13:13:26 +0100

    project: release 0.1.16

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject spootnik/kinsky "0.1.16-SNAPSHOT"
+(defproject spootnik/kinsky "0.1.16"
   :description "Kafka clojure client library"
   :plugins [[lein-codox "0.9.1"]]
   :url "https://github.com/pyr/kinsky"
