2a4ce23bdae20e1be2808d4026d590e796f0627f
Pierre-Yves Ritschard <pyr@spootnik.org>
2017-12-12 14:49:33 +0100

    Release 0.1.19
    * use duplex channel to gather in & out channel
    
    * update readme
    
    * correct glossary
    
    * Release 0.1.19 with support for Kafka 1.0.0
    
    This release aligns with the 1.0.0 kafka-clients library.
    Most changes are constrained to the asynchronous interface.
    
    Notable changes include:
    
    - Align with new polling behavior when no subscription exists?
      This fixes #19
    - Yield a single channel for the async producer
    - Optionally subscribe to a topic on startup
    - Optionally yield a duplex channel for the async consumer
    - Add support for querying the current subscription

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject spootnik/kinsky "0.1.17"
+(defproject spootnik/kinsky "0.1.19"
   :description "Kafka clojure client library"
-  :plugins [[lein-codox "0.9.1"]]
+  :plugins [[lein-codox "0.9.1"]
+            [lein-ancient "0.6.14"]]
   :url "https://github.com/pyr/kinsky"
   :license {:name "MIT License"
             :url  "https://github.com/pyr/kinsky/tree/master/LICENSE"}
@@ -13,4 +14,4 @@
   :test-selectors {:default     (complement :integration)
                    :integration :integration
                    :all         (constantly true)}
-  :profiles {:dev {:dependencies [[org.slf4j/slf4j-nop "1.7.22"]]}})
+  :profiles {:dev {:dependencies [[org.slf4j/slf4j-nop "1.7.25"]]}})
